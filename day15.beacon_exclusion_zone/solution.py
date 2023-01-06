84a6d5541a4d6f474d65a5592fa6bd937502bb5a
Guilherme Andrade <g@gandrade.net>
2023-01-06 18:54:17 +0000

    Day 15: Beacon Exclusion Zone (part 2)

--- day15.beacon_exclusion_zone/solution.py
+++ day15.beacon_exclusion_zone/solution.py
@@ -8,9 +8,16 @@ def calc_distance(pos_a, pos_b):
     x_b, y_b = pos_b
     return abs(x_a - x_b) + abs(y_a - y_b)
 
-
-queried_y = int(sys.argv[1])
-print(f'querying y={queried_y}')
+queried_y = None
+if sys.argv[1] == 'part1':
+    queried_y = int(sys.argv[2])
+    print(f'Part 1, querying y={queried_y}')
+else:
+    assert sys.argv[1] == 'part2'
+    max_distress_x = int(sys.argv[2])
+    max_distress_y = int(sys.argv[3])
+    max_distress_pos = max_distress_x, max_distress_y
+    print(f'Part 2, querying for max distress pos {max_distress_pos}')
 
 sensors_and_beacons = []
 min_x, max_x = 0, 0
@@ -44,13 +51,44 @@ for sensor, beacon, distance in sensors_and_beacons:
     sensor_bbox = (sensor_x - distance, sensor_y - distance, sensor_x + distance, sensor_y + distance)
     qtree.insert((sensor, beacon, distance), sensor_bbox)
 
-matches = qtree.intersect((min_x, queried_y, max_x, queried_y))
-excluded_count = 0
-for x in range(min_x, max_x + 1):
-    our_pos = x, queried_y
-    for sensor, beacon, distance in matches:
-        if calc_distance(our_pos, sensor) <= distance and our_pos != beacon:
-            excluded_count += 1
-            break
 
-print(f'excluded count for y={queried_y}: {excluded_count}')
+if queried_y is not None:
+    # Part 1
+    matches = qtree.intersect((min_x, queried_y, max_x, queried_y))
+    excluded_count = 0
+    for x in range(min_x, max_x + 1):
+        our_pos = x, queried_y
+        for sensor, beacon, distance in matches:
+            if calc_distance(our_pos, sensor) <= distance and our_pos != beacon:
+                excluded_count += 1
+                break
+
+    print(f'excluded count for y={queried_y}: {excluded_count}')
+
+else:
+    # Part 2
+    for y in range(0, max_distress_y + 1):
+        x = 0
+        if y % 100000 == 0:
+            print(f'progress: {100 * y / (max_distress_y + 1)} %')
+
+        while x < (max_distress_x + 1):
+            our_pos = x, y
+            # print(f'checking {our_pos}')
+
+            matches = qtree.intersect((x, y, x, y))
+            jumping = False
+            for sensor, _beacon, distance in matches:
+                if calc_distance(our_pos, sensor) <= distance:
+                    sensor_x, sensor_y = sensor
+                    y_distance = abs(sensor_y - y)
+                    next_x = sensor_x + (distance - y_distance)
+                    x = max(x, next_x)
+                    jumping = True
+
+            if not jumping and x < max_distress_x + 1:
+                tuning_freq = (x * 4000000) + y
+                print(f'found distress beacon at {(x, y)}; its tuning freq is {tuning_freq}')
+                sys.exit(0)
+            else:
+                x += 1
